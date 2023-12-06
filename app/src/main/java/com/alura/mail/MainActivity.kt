a21380ed299bdf13ef33b6790baf1b24730ef9a9
Junior <35709152+git-jr@users.noreply.github.com>
2023-12-06 18:05:19 -0300

    aula 4.2

--- app/src/main/java/com/alura/mail/MainActivity.kt
+++ app/src/main/java/com/alura/mail/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.alura.mail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.alura.mail.ui.navigation.HomeNavHost
 import com.alura.mail.ui.theme.MAILTheme
+import com.google.mlkit.nl.smartreply.SmartReply
+import com.google.mlkit.nl.smartreply.SmartReplySuggestionResult
+import com.google.mlkit.nl.smartreply.TextMessage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,6 +28,25 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
                     HomeNavHost(navController = navController)
+
+                    val conversation = mutableListOf<TextMessage>()
+                    conversation.add(
+                        TextMessage.createForLocalUser(
+                        "Hello, Good Morning", System.currentTimeMillis()))
+
+                    val smartReply = SmartReply.getClient()
+                    smartReply.suggestReplies(conversation)
+                        .addOnSuccessListener { result ->
+                            if (result.getStatus() == SmartReplySuggestionResult.STATUS_NOT_SUPPORTED_LANGUAGE) {
+                                // The conversation's language isn't supported, so
+                                // the result doesn't contain any suggestions.
+                            } else if (result.getStatus() == SmartReplySuggestionResult.STATUS_SUCCESS) {
+                                for (suggestion in result.suggestions) {
+                                    val replyText = suggestion.text
+                                    Log.i("SmartReply", replyText)
+                                }
+                            }
+                        }
                 }
             }
         }
